a11a0d69ad5ee3c5a1bb3de050514fbc3781817d
Admin <Admin@DESKTOP-SU6FN7H>
2020-07-26 23:50:46 +0200

    Added more info on the current progress

--- parser/hardcoded/grammar/GrammarTest.java
+++ parser/hardcoded/grammar/GrammarTest.java
@@ -15,6 +15,7 @@ public class GrammarTest {
 	 *       Optimized to
 	 *         S > "test" S
 	 */
+	
 	/* TODO: The grammar is not checking for unused tokens.
 	 *       If a token T is not used by any other rule it
 	 *       should be optimized away.
@@ -22,6 +23,7 @@ public class GrammarTest {
 	 *       Same goes for unused items but that requires a
 	 *       entry set to be defined.
 	 */
+	
 	/* TODO: Nesting brackets inside brackets should not be
 	 *       allowed.
 	 *       
@@ -32,8 +34,10 @@ public class GrammarTest {
 	 *       The only way to fix this is by enforcing a rule
 	 *       that you cannot nest brackets..
 	 */
+	
 	/* TODO: The square bracket allows for ambiguity when written
-	 *       in certain ways.
+	 *       in certain ways. This will break the LR(k) parser
+	 *       generator.
 	 *       
 	 *       A way to fix this is by adding another item that is
 	 *       in the form.
@@ -49,12 +53,53 @@ public class GrammarTest {
 	 *       to
 	 *       
 	 *         S > S1 ";"
-	 *         S1 > S0
-	 *            > S1 "," S0
 	 *         S0 > name "=" n
 	 *            > name
-	 *            
+	 *            > "," S0
+	 *       
+	 *       This means that any expression
+	 *       
+	 *         S > a [ b a ] $
+	 *       
+	 *       Can be written
+	 *       
+	 *         S > a S0 $
+	 *         S0 > b a S0
+	 *            > 
 	 *       By doing this we remove any ambiguity that was present
 	 *       in the later form...
+	 */
+	
+	/* TODO: Optimizations these rules should be added as a new OptimizationRule().
+	 *       This is so that if two rules uses the same syntax we can use the same
+	 *       optimization for both of them without changing the way things are parsed.
+	 *       
+	 *       For the following syntaxes
+	 *       
+	 *       1)  S > a [b a]
+	 *          
+	 *         -Becomes-
+	 *          
+	 *          S > S0
+	 *          S0 > a b S0
+	 *             > a
+	 *       
+	 *       2) S > a [b]
+	 *          
+	 *         -Becomes-
+	 *          
+	 *          S > a S0
+	 *          S0 > b S0
+	 *             > b
+	 *          
+	 *          
+	 *          
+	 *          
+	 *       3) S > a (b)
+	 *          
+	 *         -Becomes-
+	 *          
+	 *          S > a b
+	 *            > a
 	 */
 }
